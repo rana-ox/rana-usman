b5776343b655726d28ac395c9cad57a2637207df
Usman Ahmed <184513357+rana-ox@users.noreply.github.com>
2026-01-13 18:23:42 +0500

    Update Supabase configuration in env.js

--- assests/js/env.js
+++ assests/js/env.js
@@ -1,4 +1,5 @@
 window.__ENV = {
-  SUPABASE_URL: "{{SUPABASE_URL}}",
-  SUPABASE_SERVICE_ROLE_KEY: "{{SUPABASE_SERVICE_ROLE_KEY}}"
+  // public (safe in browser)
+  SUPABASE_URL: "https://tfjezyvqudlyjqwhapto.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_OWw0-kUZXm-c4MxwN0dfAg_7-r3_9nl"
 };
