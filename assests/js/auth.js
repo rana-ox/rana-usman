8398ea1e17cbe7bec473514e7e075bf8b8402f29
Usman Ahmed <184513357+rana-ox@users.noreply.github.com>
2026-01-13 15:20:52 +0500

    Create auth.js

---
+++ assests/js/auth.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  window.__https://supabase.com/dashboard/project/tfjezyvqudlyjqwhapto/settings/api-keys,
+  window.__sb_publishable_OWw0-kUZXm-c4MxwN0dfAg_7-r3_9nl
+);
